10393f0e243fe401121ed485e82244dce1c45d33
Gregoire <Gregoire@LAPTOP-EMRUUR2I>
2024-10-17 14:31:45 +0100

    Big update of the database

---
+++ MyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWeb.Data;
+using MyWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        // Constructor
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Action method for the Index view (List of Categories)
+        public IActionResult Index()
+        {
+            // Get a list of categories from the database
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        // GET: Action method to show the form for creating a new category
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult Create(Category obj)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                // Add the new category to the database
+                _db.Categories.Add(obj);
+                _db.SaveChanges(); 
+
+               
+                return RedirectToAction("Index");
+            }
+
+          
+            return View(obj);
+        }
+    }
+}
---
+++ MyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MyWeb.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyWeb.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+                        modelBuilder.Entity<Category>().HasData(
+
+
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+
+}
+
